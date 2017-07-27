c218cb2910397ac743e76d185172344ea965d5d0
maratgaip <marattig@gmail.com>
2017-07-26 23:24:31 -0700

    nested proxy added

---
+++ script.js
@@ -0,0 +1,21 @@
+(function () {
+	var proxyHandler = {
+		get: function (target, key) {
+			if (!(key in target)) {
+				target[key] = deepProxy({})
+			}
+
+			return target[key]
+		}
+	};
+
+	function deepProxy (obj) {
+		return new Proxy(obj, proxyHandler)
+	}
+
+	if (typeof module === 'undefined') {
+		this.deepProxy = deepProxy
+	} else {
+		module.exports = deepProxy
+	}
+})();
